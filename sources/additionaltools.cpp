348fa5c2b890320344f675e3aefab95dee0bb785
hellochrisyou <hellochrisyou@gmail.com>
2017-05-21 23:50:03 -0400

    2nd comment commit

--- sources/additionaltools.cpp
+++ sources/additionaltools.cpp
@@ -78,6 +78,7 @@ void AdditionalTools::resizeCanvas(int width, int height, bool flag)
 	mPImageArea->clearSelection();
 }
 
+//set default canvas 800:600
 void AdditionalTools::defaultCanvas(int width, int height, bool flag)
 {
 	width = 800;
@@ -103,6 +104,7 @@ void AdditionalTools::defaultCanvas(int width, int height, bool flag)
 	}
 }
 
+//set default canvas 1280:720
 void AdditionalTools::defaultCanvas2(int width, int height, bool flag)
 {
 	width = 1280;
